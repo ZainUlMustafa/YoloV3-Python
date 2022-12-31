a5b45aa7c62230669460b8894d47cbb9d039f18c
zainulmustafa <zain2984@gmail.com>
2022-12-31 22:25:00 +0500

    adjusted

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
-import numpy as np 
+import numpy as np
 import argparse
 import time
+
 
 def load_yolo():
     net = cv2.dnn.readNet("./model/yolov3.weights", "./model/yolov3.cfg")
@@ -13,11 +14,12 @@ def load_yolo():
     # print(layers_names)
     for i in net.getUnconnectedOutLayers():
         output_layers.append(layers_names[i-1])
-    #endfor
-    #output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
+    # endfor
+    # output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
     colors = np.random.uniform(0, 255, size=(len(classes), 3))
     return net, classes, colors, output_layers
-#enddef
+# enddef
+
 
 def load_image(img_path):
     # image loading
@@ -25,14 +27,17 @@ def load_image(img_path):
     img = cv2.resize(img, None, fx=0.4, fy=0.4)
     height, width, channels = img.shape
     return img, height, width, channels
-#enddef
+# enddef
 
-def detect_objects(img, net, outputLayers):			
-    blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(416, 416), mean=(0, 0, 0), swapRB=True, crop=False)
+
+def detect_objects(img, net, outputLayers):
+    blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(
+        416, 416), mean=(0, 0, 0), swapRB=True, crop=False)
     net.setInput(blob)
     outputs = net.forward(outputLayers)
     return blob, outputs
-#enddef
+# enddef
+
 
 def get_box_dimensions(outputs, height, width):
     boxes = []
@@ -41,10 +46,11 @@ def get_box_dimensions(outputs, height, width):
     for output in outputs:
         for detect in output:
             scores = detect[5:]
-            print(scores)
+            # print(scores)
             class_id = np.argmax(scores)
             conf = scores[class_id]
             if conf > 0.3:
+                print(class_id)
                 center_x = int(detect[0] * width)
                 center_y = int(detect[1] * height)
                 w = int(detect[2] * width)
@@ -54,13 +60,14 @@ def get_box_dimensions(outputs, height, width):
                 boxes.append([x, y, w, h])
                 confs.append(float(conf))
                 class_ids.append(class_id)
-            #endif
-        #endfor
-    #endfor
+            # endif
+        # endfor
+    # endfor
     return boxes, confs, class_ids
-#enddef
+# enddef
 
-def draw_labels(boxes, confs, colors, class_ids, classes, img): 
+
+def draw_labels(boxes, confs, colors, class_ids, classes, img):
     indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
     font = cv2.FONT_HERSHEY_PLAIN
     for i in range(len(boxes)):
@@ -68,30 +75,33 @@ def draw_labels(boxes, confs, colors, class_ids, classes, img):
             x, y, w, h = boxes[i]
             label = str(classes[class_ids[i]])
             color = colors[i]
-            cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+            cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
             cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
-        #endif
-    #endfor
+        # endif
+    # endfor
     cv2.imshow("Image", img)
-#enddef
+# enddef
 
-def image_detect(img_path): 
+
+def image_detect(img_path):
     model, classes, colors, output_layers = load_yolo()
     image, height, width, channels = load_image(img_path)
     blob, outputs = detect_objects(image, model, output_layers)
     boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+    print(class_ids)
     draw_labels(boxes, confs, colors, class_ids, classes, image)
     while True:
         key = cv2.waitKey(1)
         if key == 27:
             break
-        #endif
-    #endwhile
-#enddef
+        # endif
+    # endwhile
+# enddef
+
 
 def webcam_detect():
     model, classes, colors, output_layers = load_yolo()
-    #cap = start_webcam()
+    # cap = start_webcam()
     cap = cv2.VideoCapture(0)
     while True:
         _, frame = cap.read()
@@ -102,34 +112,37 @@ def webcam_detect():
         key = cv2.waitKey(1)
         if key == 27:
             break
-        #endif
-    #endwhile
+        # endif
+    # endwhile
     cap.release()
-#enddef
+# enddef
 
 
 def start_video(video_path):
-	model, classes, colors, output_layers = load_yolo()
-	cap = cv2.VideoCapture(video_path)
-	while True:
-		_, frame = cap.read()
-		height, width, channels = frame.shape
-		blob, outputs = detect_objects(frame, model, output_layers)
-		boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
-		draw_labels(boxes, confs, colors, class_ids, classes, frame)
-		key = cv2.waitKey(1)
-		if key == 27:
-			break
-        #endif
-    #endwhile
-	cap.release()
-#enddef
+    model, classes, colors, output_layers = load_yolo()
+    cap = cv2.VideoCapture(video_path)
+    while True:
+        _, frame = cap.read()
+        height, width, channels = frame.shape
+        blob, outputs = detect_objects(frame, model, output_layers)
+        boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+        draw_labels(boxes, confs, colors, class_ids, classes, frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+        # endif
+    # endwhile
+    cap.release()
+# enddef
+
 
 def main():
     print("Main")
     webcam_detect()
-#enddef
+    # image_detect('./data/books.jpg')
+# enddef
+
 
 if __name__ == "__main__":
     main()
-#endif
+# endif
